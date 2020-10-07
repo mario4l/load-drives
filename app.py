5ef6dfec727df234a2755f63ef8350c61117025e
mario4l <mario.vargas29@gmail.com>
2020-10-07 13:20:34 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+path_one = ''
+path_two = ''
+path_three = ''
+path_four = ''
+path_five = ''
+path_six = ''
+
+paths = [path_one, path_two, path_three, path_four, path_five, path_six]
+
+for p in paths:
+    os.startfile(p)
